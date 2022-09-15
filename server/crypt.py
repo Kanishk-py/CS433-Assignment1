d00500ef331650ce400242c36f9b8b9db49f3974
Kanishk-py <75932955+Kanishk-py@users.noreply.github.com>
2022-09-15 19:32:19 +0530

    Create crypt.py

---
+++ server/crypt.py
@@ -0,0 +1,33 @@
+def plainText(data, x):
+    return data
+
+
+def ceaserCiper(data, shift):
+    if type(data) is bytes:
+        data = list(data)
+        for i in range(len(data)):
+            data[i] = (data[i] + shift) % 256
+
+        return bytes(data)
+    else:
+        result = ""
+        for i in range(len(data)):
+            char = data[i]
+
+            if char.isupper():
+                result += chr((ord(char) + shift - 65) % 26 + 65)
+            elif char.islower():
+                result += chr((ord(char) + shift - 97) % 26 + 97)
+            elif char.isdigit():
+                result += chr((ord(char) + shift - 48) % 10 + 48)
+            else:
+                result += char
+
+    return result
+
+
+def Transpose(data, x):
+    return data[::-1]
+
+
+cryption = {"0": plainText, "1": ceaserCiper, "2": Transpose}
